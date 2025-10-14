d182e1d21f732a969ece11b4f8bc0d3423920415
student <student@corp.kpiot.ru>
2025-10-14 12:58:00 +0500

    ang

--- tasks/task6.py
+++ tasks/task6.py
@@ -2,8 +2,16 @@
 
 def solve():
 # Ниже пишите решение задачи
+    a= float(input("введите первую сторону: "))
+    b= float(input("введите вторую сторону: "))
+    c= float(input("введите третью сторону: "))
+    sides= sorted([a, b, c])
+    h = sides[2]
+    s = sides[0]
+    t = sides[1]
+    d = h**2 == s**2 + t**2
+    print(d)
 
-   
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
